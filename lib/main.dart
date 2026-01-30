30e71447318be99cd90f2b477eb0bffe18117bf4
alberto591 <albertocalvorivas@gmail.com>
2026-01-30 09:39:42 +0100

    fix: add missing cloud_firestore import

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/foundation.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:provider/provider.dart';
 import 'package:neurostack_app/config/locator.dart';
